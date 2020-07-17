a18dd7d75546af4f57773fa8b45c1ad7a2cdbade
IamVargas <68137941+IamVargas@users.noreply.github.com>
2020-07-17 01:16:56 -0500

    Create DiccionariosEnfermedades.py

---
+++ DiccionariosEnfermedades.py
@@ -0,0 +1,33 @@
+dAdison = {"DESCRIPCION":"La insuficiencia adrenal (o suprarrenal) y la enfermedad de Addison son trastornos endocrinos u hormonales en los que el organismo no produce cantidad suficiente de hormonas suprarrenales como cortisol y aldosterona. ",
+ "SINTOMAS":"Dolor abdominal, Disminución del vello corporal, Deshidratación en la enfermedad de Addison, Diarrea o estreñimiento, Mareos y desmayos, Fatiga o debilidad extrema.",
+ "PRUEBAS":"El médico sospechará una insuficiencia adrenal ante signos y síntomas como hiperpigmentación, debilidad, una presión sanguínea baja, y deseo imperioso de consumo de sal, especialmente si empeoran o son más manifiestos durante períodos de estrés. ",
+ "TRATAMIENTO":"El tratamiento de la insuficiencia adrenal supone la administración de hormonas de sustitución. El tipo de tratamiento depende de si falta cortisol, aldosterona o ambos. A veces se puede recuperar en parte la función adrenal cuando la causa de la afectación es una infección, al resolverse esta última adecuadamente."}
+
+dHepatitis = {"DESCRIPCION":"La hepatitis es una inflamación del hígado que suele ser causada por virus, aunque también puede deberse a fármacos, alcohol, enfermedades hereditarias o enfermedades autoinmunes.",
+ "SINTOMAS":"Fatiga, Náuseas, Dolor abdominal, Dolores articulares, Picor, Ictericia. ",
+ "PRUEBAS":"Pruebas generales para detectar inflamación del hígado y/o lesión hepática. ",
+ "TRATAMIENTO":"Las medidas de soporte y el alivio de los síntomas suelen ser los únicos tratamientos que se requieren para tratar las hepatitis víricas agudas A, B y C. Suele ser necesario mucho reposo y aporte de bebidas y alimentos nutritivos, puede variar respecto al tipo de Hepatitis."}
+
+dGraves = {"DESCRIPCION":"La enfermedad de Graves es la causa más frecuente de hiperactividad de la glándula tiroides (hipertiroidismo). Consiste en un trastorno autoinmune, una agresión del propio sistema inmune hacia la glándula tiroides.",
+ "SINTOMAS":"Dificultad Para Concentrarse, Visión Borrosa O Doble, Irritación Ocular Y Lagrimeo, Fatiga, Aumento Del Peristaltismo (Movimiento) Intestinal, Bocio.",
+ "PRUEBAS":"El objetivo de la realización de las pruebas es el de detectar la disfunción tiroidea, diagnosticar la enfermedad de Graves y monitorizar el tratamiento.",
+ "TRATAMIENTO":"El tratamiento va dirigido a reducir las hormonas producidas por la glándula tiroidea y a aliviar la sintomatología. Los betabloqueantes, como el propranolol, pueden atenuar las taquicardias, la sudoración y la ansiedad producidas por un aumento de la actividad hormonal. "}
+
+dGuillain = {"DESCRIPCION":"El síndrome de Guillain-Barré (SGB) es un trastorno agudo asociado a una debilidad muscular progresiva y parálisis. Se trata de un trastorno autoinmune en el que el sistema inmune del organismo ataca su propio sistema nervioso, originando una inflamación que lesiona o destruye la vaina (capa externa) de mielina de los nervios dejando desprotegidas las fibras nerviosas (axones) e incluso lesionándolas.",
+ "SINTOMAS":"Hormigueo o sensaciones como de pinchazos con alfileres o agujas, Entumecimiento, Hipersensibilidad, Debilidad, especialmente en las piernas, Espasmos musculares, Pérdida de coordinación, Pérdida de reflejos.",
+ "PRUEBAS":"La historia del paciente es muy importante para el diagnóstico. Es bastante típica la presencia de una parálisis ascendente progresiva, que empieza por manos y pies y sigue avanzando. En aproximadamente un 50% de casos existe el antecedente de una infección leve como faringitis, resfriado, gripe o diarrea.",
+ "TRATAMIENTO":"El síndrome de Guillain-Barré (SGB) suele resolverse por sí solo. La mayor parte de las veces los síntomas acabarán desapareciendo al cabo de unas semanas o unos meses. Los objetivos del tratamiento pueden resumirse en intentar disminuir la severidad de los síntomas, acelerar la recuperación y prevenir o minimizar posibles complicaciones. "}
+
+dHashimoto = {"DESCRIPCION":"La tiroiditis de Hashimoto constituye la forma más común de inflamación de la glándula tiroides (tiroiditis) y la causa más frecuente de hipotiroidismo. La tiroiditis de Hashimoto es consecuencia de un trastorno autoinmune en el que acontece un ataque de la glándula tiroides por parte del propio sistema inmune del individuo. ",
+ "SINTOMAS":"Constipación, Depresión, Sequedad de la piel, Fatiga, Pérdida de memoria, Aumento de la sensibilidad al frío",
+ "PRUEBAS":"El objetivo del tratamiento es el de detectar la disfunción tiroidea, diagnosticar la tiroiditis de Hashimoto y monitorizar el tratamiento.",
+ "TRATAMIENTO":"La tiroiditis de Hashimoto no se puede curar, aunque la enfermedad suele ser poco agresiva y fácil de controlar. Si los niveles de hormonas tiroideas T3 y T4 son normales y el individuo no presenta síntomas importantes, la mayor parte de las veces no suele ser necesario tratamiento alguno."}
+
+
+
+dEnfermedades = {"ENFERMEDAD DE ADDISON":dAdison,
+                "HEPATITIS AUTOINMUNE":dHepatitis,
+                "ENFERMEDAD DE GRAVES":dGraves,
+                "SINDROME DE GUILLAIN-BARRE":dGuillain,
+                "TIROIDITIS DE HASHIMOTO":dHashimoto}
+
