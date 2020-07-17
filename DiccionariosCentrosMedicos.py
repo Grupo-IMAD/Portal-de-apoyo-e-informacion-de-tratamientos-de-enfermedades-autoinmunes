83554377807a1f08df498d6dbebbec82dfdd8805
IamVargas <68137941+IamVargas@users.noreply.github.com>
2020-07-17 01:17:32 -0500

    Create DiccionariosCentrosMedicos.py

---
+++ DiccionariosCentrosMedicos.py
@@ -0,0 +1,68 @@
+dLima = {"NOMBRE":"Centro De Referencia Nacional De Alergia, Asma E Inmunologia",
+         "CALIFICACION":"Reseña de 8.0 sobre 10",
+         "DISTRITO":"Breña",
+         "DIRECCION":"Av. Brasil #660"}
+
+dLima2 = {"NOMBRE":"Clinica San Jose",
+         "CALIFICACION":"Reseña de 7.8 sobre 10",
+         "DISTRITO":"San Borja",
+         "DIRECCION":"Av. Metropolitana #960"}
+
+dLima3 = {"NOMBRE":"Clinica Garcilazo",
+         "CALIFICACION":"Reseña de 8.9 sobre 10",
+         "DISTRITO":"La Victoria",
+         "DIRECCION":"Av. Nueva Zelanda #540"}
+
+dArequipa =  {"NOMBRE":"Centro Medico Daniel Alcides Carreon",
+         "CALIFICACION":"Reseña de 7.5 sobre 10",
+         "DISTRITO":"Cercado - Arequipa",
+         "DIRECCION":"Calle Clorinda Matto de Turner #116"}
+
+
+dArequipa2 =  {"NOMBRE":"Centro Medico Don Fausto",
+         "CALIFICACION":"Reseña de 4.5 sobre 10",
+         "DISTRITO":"Yanahuara",
+         "DIRECCION":"Calle Girasoles #780"}
+
+dCusco =  {"NOMBRE":"Centro Medico Prana",
+         "CALIFICACION":"Reseña de 6.7 sobre 10",
+         "DISTRITO":"Cercado - Cusco",
+         "DIRECCION":"Roberto Acosta #154-A"}
+
+dCusco2 =  {"NOMBRE":"Centro Medico Americana",
+         "CALIFICACION":"Reseña de 4.7 sobre 10",
+         "DISTRITO":"Cercado - Cusco",
+         "DIRECCION":"Av. Garcilazo #1020"}
+
+dTacna =  {"NOMBRE":"Centro de Salud Leoncio Prado",
+         "CALIFICACION":"RReseña de 5.9 sobre 10",
+         "DISTRITO":"Cercado - Tacna",
+         "DIRECCION":"General Varela #1250"}
+
+dTacna2 =  {"NOMBRE":"Centro Medico Sinai",
+         "CALIFICACION":"RReseña de 6.9 sobre 10",
+         "DISTRITO":"General Basadre",
+         "DIRECCION":"Av. Independencia #1050"}
+
+dHuanuco =  {"NOMBRE":"Clinica Terapeutica Municipal",
+         "CALIFICACION":"Reseña de 4.1 sobre 10",
+         "DISTRITO":"Cercado - Huanuco",
+         "DIRECCION":"Jr. Bolivar #480"}
+
+dHuanuco2 =  {"NOMBRE":"Clinica Independencia Americana",
+         "CALIFICACION":"Reseña de 5.1 sobre 10",
+         "DISTRITO":"Huanuco",
+         "DIRECCION":"Jr. Villanueva #1020"}
+
+dCentrosMedicos = {"LIMA I":dLima,
+                   "LIMA II":dLima2,
+                   "LIMA III":dLima3,
+                "AREQUIPA I ":dArequipa,
+                "AREQUIPA II":dArequipa2,
+                "CUSCO I":dCusco,
+                "CUSCO II":dCusco2,
+                "TACNA I ":dTacna,
+                "TACNA II":dTacna2,
+                "HUANUCO I":dHuanuco,
+                   "HUANUCO II":dHuanuco2}
+
